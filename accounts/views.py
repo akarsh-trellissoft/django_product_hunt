4dbecfb8b40ecff784788c9a77391d90b41d83af
akarsh-trellissoft <kakarsh@trellissoft.com>
2020-02-12 17:48:47 +0530

    new product

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def signup(request):
+    if request.method=='POST':
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                user=User.objects.get(username=request.POST['username'])
+                return render(request,'accounts/signup.html',{'error':'username has already been taken'})
+            except User.doesNotExist:
+                user=User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')
+        else:
+            return render(request,'accounts/signup.html',{'error':'password must match'})
+    else:
+        return render(request,'accounts/signup.html')
+def login(request):
+    return render(request,'accounts/signup.html')
+def logout(request):
+    return render(request,'signup.html')
